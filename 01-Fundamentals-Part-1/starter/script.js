9b208241647bb3ab92b1897769711f73a497c229
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-18 17:50:18 -0400

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+let js = 'amazing';
+console.log(40 + 50);
+
+let firstName = "R.J.";
+console.log(firstName);
+
+let country = "USA";
+let continent = "North America";
+let population = "331.42 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
