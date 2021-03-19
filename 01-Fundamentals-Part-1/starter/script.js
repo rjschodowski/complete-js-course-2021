3f90ef8dec155243f32a92899fc3ce204a042739
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-19 16:32:01 -0400

    Add files via upload

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,26 +1,105 @@
-/*let js = 'amazing';
-console.log(40 + 50);
+// //10. Values and Variables
 
-let firstName = "R.J.";
-console.log(firstName);
 
-let country = "USA";
-let continent = "North America";
-let population = "331.42 million";
+// /*let js = 'amazing';
+// console.log(40 + 50);
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// let firstName = "R.J.";
+// console.log(firstName);
+
+// let country = "USA";
+// let continent = "North America";
+// let population = "331.42 million";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// */
+
+
+// 12. Data Types
+
+
+// // 7 Primitive Data Types
+// //Numbers, strings, booleans, (these are the three most important), undefined (empty value),null (empty value), symbol, and BigInt
+
+
+// let javascriptIsFun = (true);
+// // console.log(javascriptIsFun);
+// // console.log(typeof true);
+// // console.log(typeof 23);
+// // console.log(typeof javascriptIsFun);
+// // console.log(typeof 'whoa man');
+
+// javascriptIsFun = "'Yes!!!!!'"
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1999;
+
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+
+// 13. let, const, and var
+
+//let can be reassigned different values eg:
+
+// let age = 99. You can change that to age = 2, or any other age.
+
+//const variables cannot be changed eg: birthYear = 1981;
+	//const variables are immutable and cannot be empty, eg: const job; no se puede hacer eso!
+
+	//Rule of thumb: use const by default unless you are certain the variable's value will have to change at some point
+
+
+// 14. Basic Operators
+
+
+/*Operators - definition
+
+An operator allows us to transform values
+
+or combine multiple values
+
+and do all kinds of work with values.
+
+There are many categories of operators
+
+like mathematical operators, comparison operators
+
+logical operators, assignment operators, and many more. 
+
 */
 
-/*Data Types
-// 7 Primitive Data Types
-//Numbers, strings, booleans, (these are the three most important), undefined (empty value),null (empty value), symbol, and BigInt
-*/
 
-let javascriptIsFun = (true);
-console.log(javascriptIsFun);
-console.log(typeof true);
-console.log(typeof 23);
-console.log(typeof javascriptIsFun);
-console.log(typeof 'whoa man');
+// //Mathematical Operations
+// const now = 2055;
+// const ageRJ = now - 1981;
+// const ageAddye = now - 2011;
+// console.log(ageAddye, ageRJ);
+
+// console.log(ageRJ * 2, ageAddye / 10);
+
+// //Common Operators
+
+// // Common operators: +, -, *, **, =, +=, -=, ++, --, *=, etc
+
+// //Comparison Operators
+
+// // <, >, >=, <=
+
+// console.log(ageRJ >= ageAddye);
+
+// console.log(now - 1981 > now - 2011);
+
+
+//15. Operator Precedence
+
+
+const now = 2055;
+const ageRJ = now - 1981;
+const ageAddye = now - 2011;
