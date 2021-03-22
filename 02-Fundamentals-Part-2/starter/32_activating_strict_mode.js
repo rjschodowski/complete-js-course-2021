b4ed342c88f501c314dada2f38cb1e0eb03195b9
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-22 15:26:47 -0400

    Add files via upload

---
+++ 02-Fundamentals-Part-2/starter/32_activating_strict_mode.js
@@ -0,0 +1,37 @@
+// Activating Strict Mode
+
+/*
+So strict mode is a special mode
+
+that we can activate in JavaScript,
+
+which makes it easier for us
+
+to write a secure JavaScript code.
+
+And all we have to do to activate strict mode
+
+is to write this string at the beginning of the script:
+
+
+'use strict';
+
+
+
+First - strict mode forbids us to do certain things
+
+Second - it creates visible errors
+
+and the developer console, where in other situations
+
+JavaScript would just fail silently.
+
+
+//Strict mode reserves certain words for possible future JavaScript usage, and it will create an error message to let you know if you choose a reserved word
+
+
+
+
+
+
+*/
