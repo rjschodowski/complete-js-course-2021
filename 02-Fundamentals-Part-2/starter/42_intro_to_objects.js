a72cee15c3a7165b9da69e260bff9632d38e5a71
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-25 16:52:03 -0400

    Add files via upload

---
+++ 02-Fundamentals-Part-2/starter/42_intro_to_objects.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//an OBJECT is data structure
+
+//Object example
+
+// const aboutRj {
+//     firstName: 'R.J.',
+//     lastName: 'Schodowski',
+//     age: 2021 - 1981,
+//     job: 'web developer',
+//     friends: ['Huri', 'David', 'Dan']
+// }
+
+/*
+
+The big difference between objects and arrays,
+
+is that in objects, the order of these values
+
+does not matter at all when we want to retrieve them.
+
+And that's important to keep in mind.
+
+
+We should use arrays for more order data,
+
+and objects for more unstructured data.
+
+And data that we actually want to name,
+
+and then retrieve from the object,
+
+based on that name.
+
+*/
+
