6e22be3324e5897db6fd78edb6e9a27f75a2d120
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-28 14:16:21 -0400

    Add files via upload

---
+++ 02-Fundamentals-Part-2/starter/50_code_challenge_number_4.js
@@ -0,0 +1,39 @@
+/*
+Coding Challenge #4
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays
+*/
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+const calcTip = function (bill) {
+    const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+    return tip;
+}
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = []; //this will contain the calculated ages in an array
+// for (let i = 0; i < years.length; i++) {
+//     ages.push(2021 - years[i]);
+// }
+
+// console.log(ages);
