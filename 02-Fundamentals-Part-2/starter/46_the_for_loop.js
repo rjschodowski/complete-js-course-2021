f19cb5ab60f2a7137000e494029a0bf7152d317a
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-28 09:36:07 -0400

    Add files via upload

---
+++ 02-Fundamentals-Part-2/starter/46_the_for_loop.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Just as the 'if/else' statement, LOOPS are also CONTROL STRUCTURES.
+
+//LOOPS allow us to automate tasks we have to perform repetitively.
+
+
+//For loop keeps running while condition is TRUE. Also, remember that rep ++ is = rep = rep + 1// {}
+for(let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}.`);
+}
