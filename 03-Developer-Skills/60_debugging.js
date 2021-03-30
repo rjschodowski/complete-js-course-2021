eaf1b1c9ad82be6000b959d01ba091fb87da5e5f
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-30 17:08:25 -0400

    Add files via upload

---
+++ 03-Developer-Skills/60_debugging.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/*
+A software bug is a defect or problem in a computer program. It is any unexpected or unintended behavior of a
+computer program.
+*/
+
+//Debugging:
+
+//Identify the bug
+
+    //During developing
+    //Testing software
+    //User reports during production
+    //Context: browsers, users, etc.
+
+//Find the bug
+
+    //developer console (simple code)
+    //debugger (complex code)
+
+//Fix the bug
+
+    //replace wrong solution with the correct solution
+
+//Prevent bugs from happening again
+
+    //search for the same bug in similar code
+    //write tests using testing software
