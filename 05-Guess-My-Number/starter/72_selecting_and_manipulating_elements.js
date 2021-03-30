dbeb6579d39b93312204cb63a147cba5d682fcbf
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-30 19:13:25 -0400

    Add files via upload

--- 05-Guess-My-Number/starter/72_selecting_and_manipulating_elements.js
+++ 05-Guess-My-Number/starter/72_selecting_and_manipulating_elements.js
@@ -3,3 +3,14 @@
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct number, dude!!!;'
 console.log(document.querySelector('.message').textContent = 'Correct number, dude!!!');
+
+//Let's manipulate the number and score classes now:
+
+document.querySelector('.number').textContent = 777;
+document.querySelector('.score').textContent = 5994321;
+
+//Now let's manipulate the input field (class is guess)
+    //.value is used to access and manipulate input fields
+
+document.querySelector('.guess').value = 999;
+console.log(document.querySelector('.guess').value);
