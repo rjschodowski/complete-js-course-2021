60bfc23756e31f70f9296d849ee9a967bff92569
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-30 19:03:49 -0400

    Add files via upload

---
+++ 05-Guess-My-Number/starter/71_whats_DOM_and_DOM_manipulation.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//the DOM always starts with the document object.
+
+/*And document is a special object that we have access to in JavaScript. And this object serves as an entry point
+into the DOM. Document is the entry point to the DOM, because we need it to start selecting elements.
+
+The DOM and DOM methods are actually part of something called the web APIs. So the web API APIs are like libraries
+that browsers implement and that we can access from our JavaScript code.
+
+Besides the DOM, there are actually a ton more web APIs, such as timers, the fetch API, and many more.*/
