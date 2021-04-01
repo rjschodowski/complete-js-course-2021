90925f6f203aca3ac1e9321b736f292ea16319e1
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-04-01 17:24:11 -0400

    Update 79_project_2_modal_window.js

--- 06-Modal/starter/79_project_2_modal_window.js
+++ 06-Modal/starter/79_project_2_modal_window.js
@@ -1,1 +1,13 @@
 //Manipulating classes is the main way we manipulate webpages
+
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+console.log(btnsOpenModal[i].textContent);
