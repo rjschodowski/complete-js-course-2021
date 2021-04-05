d0a1b3d62a463744d3d2af7a06850f1873f58454
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-04-05 17:33:41 -0400

    Add files via upload

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,10 +13,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0]
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+//Starting conditionss
+const init = function() {
+    const scores = [0, 0]
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1EL.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -26,18 +43,12 @@ const switchPlayer = function() {
     player1El.classList.toggle('player--active');
 };
 
-//Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
 //Rolling dice functionality
 
 btnRoll.addEventListener('click', function () {
     if (playing) {
         //1. Generate dice roll
         const dice = Math.trunc(Math.random() * 6) + 1;
-        console.log(dice);
 
         //2. Display dice
         diceEl.classList.remove('hidden');
@@ -75,3 +86,5 @@ btnHold.addEventListener('click', function() {
         }
     }
 });
+
+btnNew.addEventListener('click', init);
