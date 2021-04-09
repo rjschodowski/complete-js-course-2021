b602651eaa72ce6295529ece0a620d68b111e87b
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-04-09 16:57:01 -0400

    Add files via upload

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,75 +1,104 @@
 'use strict'
 
-//Nullish Coalescing Operator
+///////////////////////////////////////
+// Coding Challenge #1
 
-//There is a problem with the number of guests if the number is zero. As shown below, when using the || shortcircuit, the
-//result is 10 instead of 0. There is a solution to this type of problem below...
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
 
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-//Restaurant example//
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array
+('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total 
+number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
-
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery: function ({ starterIndex, mainIndex, time, address}) {
-    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}. Watch for the flashy schoolbus!`);
-  },
-
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your tasty pasta with ${ing1}, ${ing2}, and ${ing3}.`);
-  },
-  orderPizza: function (mainIngredient, ...otherIngredients) {        //Rest operator example here for ordering pizza
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  }
 };
 
-console.log('---------------HERE IS THE PROBLEM------------------------------');
+//1
+const [players1, players2] = game.players;
+console.log(players1, players2);
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests); //console prints 10 instead of 0.
+//2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
 
- console.log('---------------HERE IS THE SOLUTION------------------------------');
+//3
 
- const guestCorrect = restaurant.numGuests ?? 10;
- console.log(guestCorrect);
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
 
-/*So why does this work? It is because the nullish coalescing operator
+//4
 
-works with the idea or with the concept
+const players1Final = [...players1, 'Thiago','Coutinho', 'Perisic'];
+console.log(players1Final);
 
-of nullish values instead of falsy values.
+//5
 
-And nullish values are null and undefined.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
 
-That's it.*/
+//6'
 
-//NULLISH VALUES = null and undefined (NOT 0 or '')
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored.`);
+};
 
+printGoals(...game.scored);
 
+//7 
 
-
-
-
+team1 < team2 && console.log('Team 1 is more likely to win.');
+team1 > team2 && console.log('Team 2 is more likely to win.');
