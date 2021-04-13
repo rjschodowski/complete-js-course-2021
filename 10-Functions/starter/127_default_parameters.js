fab3858762021e00a97dbd044ce88532363672b3
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-04-13 17:25:07 -0400

    Add files via upload

---
+++ 10-Functions/starter/127_default_parameters.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//Functions - Default Parameters
+
+const bookings = [];
+
+const createBooking = function (
+    flightNum,
+    numPassengers = 1,
+    price = 199 * numPassengers
+) {
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000); //Use UNDEFINED if you don't want to pass a value to a parameter
